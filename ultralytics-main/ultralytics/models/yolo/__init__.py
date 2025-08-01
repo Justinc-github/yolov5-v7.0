8eb76dbc56ff964c7aa1287a91f8f3beb76662f0
justinc <1927705375@qq.com>
2025-08-01 20:02:03 +0800

    Yolov5

---
+++ ultralytics-main/ultralytics/models/yolo/__init__.py
@@ -0,0 +1,5 @@
+# Ultralytics YOLO 🚀, AGPL-3.0 license
+
+from .model import YOLO, YOLOWorld
+
+__all__ = "classify", "segment", "detect", "pose", "obb", "world", "YOLO", "YOLOWorld"
---
+++ ultralytics-main/ultralytics/models/yolo/classify/__init__.py
@@ -0,0 +1,3 @@
+# Ultralytics YOLO 🚀, AGPL-3.0 license
+
+__all__ = "ClassificationPredictor", "ClassificationTrainer", "ClassificationValidator"
